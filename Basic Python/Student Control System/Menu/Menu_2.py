4249af91d6fc021a660f0a5cba9359f73eab3e67
Wilson Fung Leung <wilsonfungliangcr@gmail.com>
2026-07-08 23:29:22 -0600

    Finished adding the function that shows top 3 students with best average grades

--- Basic Python/Student Control System/Menu/Menu_2.py
+++ Basic Python/Student Control System/Menu/Menu_2.py
@@ -2,6 +2,7 @@ from Actions.Students import student_inputer
 from Actions.Student_Info import student_printer
 from Actions.Student_Average import average_students
 from My_Data.exporter import csv_creator
+from Actions.Top_3 import top3_maker
 
 def my_menu2(my_list):
     print("Menu")
@@ -10,10 +11,11 @@ def my_menu2(my_list):
     print("2-Ver nota promedio de cada estudiantes")
     print("3-Mostrar el top 3 de estudiantes")
     print("4-Exportar datos")
+    print("5-Finalizar operacion")
     action_2=None
     try:
         action_2= int(input("Ingrese la operacion que desea hacer "))
-        if action_2 <1 or action_2>4:
+        if action_2 <1 or action_2>5:
             raise ValueError()
     except ValueError:
             print("Ingrese una opcion valida")
@@ -23,7 +25,14 @@ def my_menu2(my_list):
     elif action_2==2:
         average_students(my_list)
         my_menu2(my_list)
+    elif action_2==3:
+        top_3=top3_maker(my_list)
+        for student in top_3:
+            print(student)
+        my_menu2(my_list)
     elif action_2==4:
-        path= input("Ingrese el nombre que desea ponerle a la lista") + ".csv"
+        path= input("Ingrese el nombre que desea ponerle a la lista" ) + ".csv"
         csv_creator(path,my_list)
         my_menu2(my_list)
+    elif action_2==5:
+        exit()
